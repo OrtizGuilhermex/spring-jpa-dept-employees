aed8ab91c14377cdd171ad3f4626a9cdf15d5c70
luiz_gf_ortiz <luiz_gf_ortiz@estudante.sesisenai.org.br>
2026-03-20 20:54:58 -0300

    feat: adiciona dependencias

---
+++ src/main/java/com/ctw/spring_jpa_dept_employees/SpringJpaDeptEmployeesApplication.java
@@ -0,0 +1,13 @@
+package com.ctw.spring_jpa_dept_employees;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaDeptEmployeesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJpaDeptEmployeesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ctw/spring_jpa_dept_employees/SpringJpaDeptEmployeesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ctw.spring_jpa_dept_employees;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaDeptEmployeesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
